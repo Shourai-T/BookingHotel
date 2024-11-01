74211b0b97bbb7021a7f8d776174ae74b3e46f89
Dat184 <thanhnguyendat184@gmail.com>
2024-11-01 07:00:07 +0700

    Display username and email in Nav of Staff

--- src/layout/MainLayoutStaff.js
+++ src/layout/MainLayoutStaff.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import ToggleNavStaff from '../components/staff/ToggleNavStaff'; // Assuming this is the correct path for the component
+import { useSelector } from 'react-redux';
 
 const MainLayoutStaff = () => {
+    const user = useSelector((state) => state.auth.login.currentUser);
+
     return (
         <div style={{ display: 'flex' }}>
             {/* Sidebar is always on the left and fixed */}
-            <ToggleNavStaff />
+            <ToggleNavStaff username={user.user.name} email={user.user.email}/>
             {/* Main content adjusted with left margin to not overlap the nav */}
             <div id="main" style={{ marginLeft: '250px', padding: '20px', width: '100%' }}>
                 <Outlet /> {/* This is where child routes will be rendered */}
